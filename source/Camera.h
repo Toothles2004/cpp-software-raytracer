6499d54082a4acd0bad57f3c6268e0736d717888
Leen <leentje2004@hotmail.com>
2023-10-02 16:11:21 +0200

    implemented WASD camera movement
    check if arrow or wasd keys are used, determines positive or negative movement for the axis, multiply by the vector and then multiply with the speed to determine the movement of the camera

--- source/Camera.h
+++ source/Camera.h
@@ -22,8 +22,8 @@ namespace dae
 		Vector3 origin{};
 		float fovAngle{45.f};
 
-		//Vector3 forward{Vector3::UnitZ};
-		Vector3 forward{ 0.266f, -0.453f, 0.860f };
+		Vector3 forward{Vector3::UnitZ};
+		//Vector3 forward{ 0.266f, -0.453f, 0.860f };
 		Vector3 up{ Vector3::UnitY };
 		Vector3 right{ Vector3::UnitX };
 
@@ -33,6 +33,7 @@ namespace dae
 		Matrix cameraToWorld{};
 
 		float fov{ std::tanf(TO_RADIANS*(fovAngle/2.f)) };
+		float moveSpeed{ 10 };
 
 
 		Matrix CalculateCameraToWorld()
@@ -51,23 +52,37 @@ namespace dae
 
 		void Update(Timer* pTimer)
 		{
-			float initFovAngle{ fovAngle };
+			const float initFovAngle{ fovAngle };
 			const float deltaTime = pTimer->GetElapsed();
 
 			//Keyboard Input
 			const uint8_t* pKeyboardState = SDL_GetKeyboardState(nullptr);
 
-
 			//Mouse Input
 			int mouseX{}, mouseY{};
 			const uint32_t mouseState = SDL_GetRelativeMouseState(&mouseX, &mouseY);
+
+			if((pKeyboardState == nullptr) && (mouseState & SDL_BUTTON_LMASK) && (mouseState & SDL_BUTTON_RMASK))
+			{
+				return;
+			}
+
+
+			// calculates the forward and right vector for camera movement
+			origin += moveSpeed * deltaTime * ((
+				(pKeyboardState[SDL_SCANCODE_UP] | pKeyboardState[SDL_SCANCODE_W]) -        
+				(pKeyboardState[SDL_SCANCODE_DOWN] | pKeyboardState[SDL_SCANCODE_S])		
+				) * forward + (																//calculates the forward movement vector
+				(pKeyboardState[SDL_SCANCODE_D] | pKeyboardState[SDL_SCANCODE_RIGHT]) -		
+				(pKeyboardState[SDL_SCANCODE_A] | pKeyboardState[SDL_SCANCODE_LEFT])		
+				) * right);																	//calculates the horizontal movement vector
 
 			if(std::abs(initFovAngle - fovAngle) >= 0.00001f)
 			{
 				fov = std::atanf(fovAngle);
 			}
+			
 			//todo: W2
-			//assert(false && "Not Implemented Yet");
 		}
 	};
 }
